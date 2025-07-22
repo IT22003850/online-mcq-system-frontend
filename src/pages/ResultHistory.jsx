750fa781ed0748c6a7e448a8ee8b645d297a7ed6
Thilina Thushara <thilina.sam.a7@gmail.com>
2025-07-22 17:23:35 +0530

    toml added

--- src/pages/ResultHistory.jsx
+++ src/pages/ResultHistory.jsx
@@ -4,6 +4,7 @@ import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
 
+
 function ResultHistory() {
   const { user } = useContext(AuthContext);
   const [results, setResults] = useState([]);
